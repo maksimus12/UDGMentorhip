cb714c96179ec0d25743bff9348b2ce8174d69b6
Media UDG <mediaudg@Medias-MacBook-Pro.local>
2025-01-09 21:13:32 +0200

    Extract a Form Validation Object

---
+++ Http/Forms/LoginForm.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Http\Forms;
+use Core\Validator;
+class LoginForm
+{
+    protected $errors = [];
+    public function validate($email, $password)
+    {
+
+        if(!Validator::email($email)){
+            $this->errors['email'] = 'Provide a valid email';
+        }
+
+        if(!Validator::string($password)){
+            $this->errors['password'] = 'Provide a valid password';
+        }
+
+     return empty($this->errors);
+    }
+
+    public function errors(){
+        return $this->errors;
+    }
+
+}
