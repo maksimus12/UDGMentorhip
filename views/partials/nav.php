d7b8942fbc72ae2a6a8f4f1c06fe470b35a9dcd9
Radu Aftinescu <r.aftinescu@adtelligent.com>
2025-01-13 22:48:34 +0200

    fix space

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -13,7 +13,7 @@
                         <?php if($_SESSION['user'] ?? false) :?>
                             <a href="/meetings" class="<?= urlIs('/meetings') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Meetings</a>
                         <?php endif?>
-                        <?php if(((int)($_SESSION['user']['user_role'] ?? null))=== \Core\UserRoles::ADMIN) :?>
+                        <?php if(((int)($_SESSION['user']['user_role'] ?? null)) === \Core\UserRoles::ADMIN) :?>
                             <a href="/students" class="<?= urlIs('/students') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Students</a>
                         <?php endif?>
 <!--                        <a href="/contact" class="--><?php //= urlIs('/contact') ? 'bg-gray-900 text-white' : 'text-gray-300' ?><!-- hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>-->
